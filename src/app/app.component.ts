6498c7ac47783eabd21df3cc89c7e3c1375be410
shonez-mohamed <shoneezmohamed8@gmail.com>
2024-12-27 16:03:12 +0200

    Start Framework

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent, AboutComponent, PortfolioComponent, ContactComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'startFramework';
+}
